a6dd940199e47e34bdd024218db6defa9f8e363c
Win Min <winminthuzar1@gmail.com>
2026-07-14 16:12:12 +0800

    Add Home component with DataTable

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import DataTable from "../components/DataTable";
+
+export default function Home() {
+  return (
+    <div className="home">
+
+      <div className="topbar">
+
+        <h2>Sohanur Cash Book</h2>
+
+      </div>
+
+      <DataTable />
+
+    </div>
+  );
+}
